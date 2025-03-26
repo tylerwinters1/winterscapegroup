e06ff5bbe5627f5c064e0561ea155664c924c2f3
tylerwinters1 <tyjwinters@icloud.com>
2025-03-26 11:36:20 -0400

    Create index.vue

---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="p-8 text-center">
+    <h1 class="text-3xl font-bold">Welcome to Winterscale CRM ✨</h1>
+    <p class="mt-2 text-gray-500">Start by visiting <NuxtLink to="/help">Help Center</NuxtLink> or <NuxtLink to="/posts">Posts</NuxtLink></p>
+  </div>
+</template>
